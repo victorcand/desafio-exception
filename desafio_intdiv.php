2cebe5e85ddedcec4747b6fe925414aa714989c1
victorcand <71890507+victorcand@users.noreply.github.com>
2021-02-13 20:40:23 -0300

    Atualização com Dicas
    Depois de ver o resultado com o professor, fiz algumas alterações.

--- desafio_intdiv.php
+++ desafio_intdiv.php
@@ -1,36 +1,32 @@
-<?php namespace Desafio ?>
+<?php namespace Desafio ;
+
+use Exception;
+?>
+
 <div class="titulo">Desafio intdiv()</div>
 
 <?php
-class Excessoes extends Exception
-{
-    public function __construct($message, $code = 0, $previus = null)
-    {
-        parent::__construct($message, $code, $previus);
-    }
-}
+
+class NaoInteiroException extends Exception{}
 
 function intdiv($num1, $num2)
 {
-    $num1 / $num2;
     if ($num2 == 0) {
-        throw new Excessoes('Divisão por zero!');
-    }
-    $resultado = $num1 % $num2;
-    if ($resultado != 0) {
-        throw new Excessoes('A divisão dos numeros não da um numero inteiro!!');
+        throw new \DivisionByZeroError('Divisão por zero!');
     }
 
-    if ($resultado == 0) {
-        echo "O resultado é: " . $num1 / $num2;
+    $resultado = $num1 % $num2;
+    if ($resultado > 0) {
+        throw new NaoInteiroException('A divisão dos numeros não da um numero inteiro!!');
     }
+
+    return "O resultado é: " . $num1 / $num2;
 }
 
 try {
-    $teste->intdiv(8, 0);
-} catch (Excessoes $e) {
+    echo intdiv(8, 2);
+} catch (NaoInteiroException $e) {
     echo $e->getMessage();
 }
 
 echo "<br>" . \intdiv(8, 3);
-
